df06c3fb388d1268d0913e1f1d0d53ae6ea8ed35
Torin Miner <torin.miner@gmail.com>
2022-01-05 16:04:02 -0600

    added 'shell' to database query

--- city3d/tasks.py
+++ city3d/tasks.py
@@ -13,7 +13,7 @@ from sqlalchemy import create_engine
 
 # @app.on_after_configure.connect
 # @periodic_task(60)
-def everyday(request):
+def main():
     AUS_active_permits()
     AUS_closed_permits()
 
@@ -29,17 +29,35 @@ def AUS_active_permits():
     # Austin Construction Permits Dataset
     data_set = "3syk-w9eu"
 
-    # Count number of active permits
-    count = client.get(data_set, where="permittype='BP' AND work_class='New' AND status_current='Active'", select="COUNT(*)")
+    search_results = "permit_type_desc,permit_class_mapped,issue_date,issued_in_last_30_days,status_current,expiresdate,work_class,original_address1,original_city,original_state,original_zip,link,project_id,latitude,longitude"
 
-    # Find all active building permits for new construction
+    # Count number of active 'NEW' construction permits
+    count = client.get(data_set, where="permittype='BP' AND status_current='Active' AND work_class='New'", select="COUNT(*)")
+
+    # Find all active building permits for 'NEW' construction
     # Iterate over dataset thanks to https://holowczak.com/getting-started-with-nyc-opendata-and-the-socrata-api/5/
     start = 0
     chunk_size = 50000
     results = []
     while True:
         # First 50000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
-        results.extend(client.get(data_set, where="permittype='BP' AND work_class='New' AND status_current='Active'", select="permit_type_desc,permit_class_mapped,issue_date,issued_in_last_30_days,status_current,expiresdate,original_address1,original_city,original_state,original_zip,link,project_id,latitude,longitude", offset=start, limit=chunk_size))
+        results.extend(client.get(data_set, where="permittype='BP' AND status_current='Active' AND work_class='New'", select=search_results, offset=start, limit=chunk_size))
+        # Shift query to next chunk
+        start = start + chunk_size
+        # If all records fetched, end loop
+        if (start > int(count[0]['COUNT'])):
+            break
+
+    # Count number of active 'SHELL' construction permits
+    count = client.get(data_set, where="permittype='BP' AND status_current='Active' AND work_class='Shell'", select="COUNT(*)")
+
+    # Find all active building permits for 'Shell' construction
+    # Iterate over dataset thanks to https://holowczak.com/getting-started-with-nyc-opendata-and-the-socrata-api/5/
+    start = 0
+    chunk_size = 50000
+    while True:
+        # First 50000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
+        results.extend(client.get(data_set, where="permittype='BP' AND status_current='Active' AND work_class='Shell'", select=search_results, offset=start, limit=chunk_size))
         # Shift query to next chunk
         start = start + chunk_size
         # If all records fetched, end loop
@@ -79,30 +97,49 @@ def AUS_closed_permits():
     # Austin Construction Permits Dataset
     data_set = "3syk-w9eu"
 
-    #Count number of active permits
-    count = client.get(data_set, where="permittype='BP' AND work_class='New' AND status_current!='Active'", select="COUNT(*)")
+    search_results = "permit_type_desc,permit_class_mapped,issue_date,issued_in_last_30_days,status_current,expiresdate,work_class,original_address1,original_city,original_state,original_zip,link,project_id,latitude,longitude"
 
-    # Find all closed building permits for new construction
+    #Count number of closed building permits for 'NEW' construction
+    count = client.get(data_set, where="permittype='BP' AND status_current!='Active' AND work_class='New'", select="COUNT(*)")
+
+    # Find all closed building permits for 'NEW' construction
     # Iterate over dataset thanks to https://holowczak.com/getting-started-with-nyc-opendata-and-the-socrata-api/5/
     start = 0
     chunk_size = 50000
     results = []
     while True:
         # First 50000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
-        results.extend(client.get(data_set, where="permittype='BP' AND work_class='New' AND status_current='Final'", select="permit_type_desc,permit_class_mapped,issue_date,issued_in_last_30_days,status_current,expiresdate,original_address1,original_city,original_state,original_zip,link,project_id,latitude,longitude", offset=start, limit=chunk_size))
+        results.extend(client.get(data_set, where="permittype='BP' AND status_current!='Active' AND work_class='New'", select=search_results, offset=start, limit=chunk_size))
         # Shift query to next chunk
         start = start + chunk_size
         # If all records fetched, end loop
         if (start > int(count[0]['COUNT'])):
             break
 
-    # Convert list to pandas DataFrame
+
+    #Count number of closed building permits for 'Shell' construction
+    count = client.get(data_set, where="permittype='BP' AND status_current!='Active' AND work_class='Shell'", select="COUNT(*)")
+
+    # Find all closed building permits for 'Shell' construction
+    # Iterate over dataset thanks to https://holowczak.com/getting-started-with-nyc-opendata-and-the-socrata-api/5/
+    start = 0
+    chunk_size = 50000
+    while True:
+        # First 50000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
+        results.extend(client.get(data_set, where="permittype='BP' AND status_current!='Active' AND work_class='Shell'", select=search_results, offset=start, limit=chunk_size))
+        # Shift query to next chunk
+        start = start + chunk_size
+        # If all records fetched, end loop
+        if (start > int(count[0]['COUNT'])):
+            break
+
+    # Convert results list to pandas DataFrame
     df = pd.DataFrame.from_records(results)
 
     # print(df)
 
     # Retrieve database url
-    database_url = 'postgresql://postgres:' + 'Austin2019' + '@localhost:5432/Buildings_Austin'
+    database_url = settings.DATABASE_URL
 
     # Save to Postgres (faster than 'to_sql' https://stackoverflow.com/questions/23103962/how-to-write-dataframe-to-postgres-table)
     engine = create_engine(database_url)
@@ -117,3 +154,54 @@ def AUS_closed_permits():
     contents = output.getvalue()
     cur.copy_from(output, 'closed_permits', null="") # null values become ''
     conn.commit()
+
+
+def the_independent_test():
+
+    # App token authentication
+    client = Socrata("data.austintexas.gov",'YoQv3uVAF8Q6UuI4ZzWRUO54Z')
+
+    # Austin Construction Permits Dataset
+    data_set = "3syk-w9eu"
+
+    #Count number of active permits
+    count = client.get(data_set, where="latitude='30.26775294' AND longitude='-97.75093592'", select="COUNT(*)")
+
+    # Find all closed building permits for new construction
+    # Iterate over dataset thanks to https://holowczak.com/getting-started-with-nyc-opendata-and-the-socrata-api/5/
+    start = 0
+    chunk_size = 50000
+    results = []
+    while True:
+        # First 50000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
+        results.extend(client.get(data_set, where="latitude='30.26775294' AND longitude='-97.75093592'", select="permit_type_desc,permit_class_mapped,issue_date,issued_in_last_30_days,status_current,expiresdate,work_class,original_address1,original_city,original_state,original_zip,link,project_id,latitude,longitude", offset=start, limit=chunk_size))
+        # Shift query to next chunk
+        start = start + chunk_size
+        # If all records fetched, end loop
+        if (start > int(count[0]['COUNT'])):
+            break
+
+    # Convert list to pandas DataFrame
+    df = pd.DataFrame.from_records(results)
+
+    print(df)
+
+    # Retrieve database url
+    database_url = 'postgresql://postgres:' + 'Austin2019' + '@localhost:5432/Buildings_Austin'
+
+    # Save to Postgres (faster than 'to_sql' https://stackoverflow.com/questions/23103962/how-to-write-dataframe-to-postgres-table)
+    engine = create_engine(database_url)
+
+    df.head(0).to_sql('the_independent', engine, if_exists='replace', index=False)
+
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    df.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    contents = output.getvalue()
+    cur.copy_from(output, 'the_independent', null="") # null values become ''
+    conn.commit()
+
+if __name__ == "__main__":
+    main()
