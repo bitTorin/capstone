1a79245d1b10403d0cde2308fee91c4a6e7b0237
Torin Miner <torin.miner@gmail.com>
2022-05-23 11:33:56 -0500

    database configuration

--- capstone/settings.py
+++ capstone/settings.py
@@ -89,10 +89,11 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': str(os.getenv('DATABASE_URL')),
 }
 
 # Password validation
